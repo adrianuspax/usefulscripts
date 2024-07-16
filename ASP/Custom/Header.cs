3537647f58c75fbdd03cbfe807704286b6d0c7ba
Adriano Paz <contato@adrianopaz.com>
2024-07-16 19:07:35 +0900

    Inserted Class Header

---
+++ ASP/Custom/Header.cs
@@ -0,0 +1,33 @@
+public static class Header
+{
+    public const string lines = " ---------------------------------------------------------------------------------------------------- ";
+    public const string equals = " ============================== ";
+    public const string MANAGEABLE = equals + "MANAGEABLE" + equals;
+    public const string READONLY = equals + "READ ONLY" + equals;
+    public const string UI = "USER INTERFACE" + lines;
+    public const string ANIMATIONS = "ANIMATIONS" + lines;
+    public const string components = "COMPONENTS" + lines;
+    public const string variables = "VARIABLES" + lines;
+    public const string arrays = "ARRAYS";
+    public const string lists = "LISTS";
+    public const string booleans = "..Booleans..";
+    public const string integers = "..Integers..";
+    public const string floaters = "..Floaters..";
+    public const string images = "..Images..";
+    public const string sprites = "..Sprites..";
+    public const string buttons = "..Buttons..";
+    public const string animators = "..Animator Controllers..";
+    public const string animationClips = "..Animation Clips..";
+    public const string audioSources = "..Audio Sources..";
+    public const string audioClips = "..Audio Clips..";
+    public const string gameObjcts = "..Game Objects..";
+    public const string TMPro = "..Texts Mesh Pro..";
+    public const string texts = "..Texts (Legacy)..";
+    public const string transforms = "..Transforms..";
+    public const string enums = "..Enumerations..";
+    public const string scripts = "SCRIPTS" + lines;
+    public const string colors = "..Colors..";
+    public const string vectors = "..Vectors..";
+    public const string linesRenderer = "..Lines Renderer..";
+    public const string prefabs = "..Prefabricated..";
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 1.0.0 Launch
 1.0.1 Improving, cleaning and documenting code
+1.0.2 Inserted Class Header
